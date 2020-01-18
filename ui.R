367e2b2785537a642db6c0f44fa8054505122d2f
Ruoyu-Wang108 <865524384@qq.com>
2020-01-18 13:38:49 -0800

    added diet input

--- ui.R
+++ ui.R
@@ -1,16 +1,24 @@
 library(shiny)
+library(tidyverse)
 library(shinythemes)
+library(here)
+library(janitor)
+
+diet_carbon <- readr::read_csv(here::here("data", "diet_carbon_emission.csv")) %>% 
+  janitor::clean_names() %>% 
+  mutate(type = ifelse(type == "vegan", "Vegan", type))
 
 # Define UI for miles per gallon application
 shinyUI(fluidPage(theme = "bootstrap.css",
-                  pageWithSidebar(
-                    
-                    # Application title
-                    headerPanel("Green Track"),
-                    
-                    sidebarPanel("Here's the input"),
-                    
-                    mainPanel("here's the output")
-                                  )
-                  )
-        )
+                  titlePanel("here is my title"),
+                  sidebarLayout(
+                    sidebarPanel("my widgets are here",
+                                 selectInput(inputId = "diet_type",
+                                             label = "Choose the type of your meal:",
+                                             choices = unique(diet_carbon$type)
+                                             )
+                                  ),
+                    mainPanel("My outputs are here!",
+                              tableOutput(outputId = "candy_table"))
+                                )
+        ))
